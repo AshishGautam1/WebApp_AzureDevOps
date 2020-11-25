2d69740e7d109bf759e08cd5ceabc2a8276dd832
Gautam <Ashish.Gautam@cognizantog.com>
2020-11-25 10:58:00 +0530

    Add project files.

---
+++ WebApp_AzureDevOps/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_AzureDevOps
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
